5b0272dc7bc5fe04d970e6727b3faee9ac89efe6
hao wang <ge_nah@hotmail.com>
2019-04-08 21:50:56 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(array)
-  puts array
-  return array
+  if array = ""
+    return "Squawk!"
+  else
+    puts array
+    return array
+  end
 end
